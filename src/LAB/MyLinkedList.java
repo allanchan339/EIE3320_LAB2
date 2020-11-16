f714c638a9af6aa0a10c1771f404c684b9139364
allanchan <allanchan339@gmail.com>
2020-11-16 20:23:52 +0800

    Initial commit

---
+++ src/LAB/MyLinkedList.java
@@ -0,0 +1,191 @@
+package LAB;
+//import MyLinkedList.Node;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class MyLinkedList<E> implements MyList<E> {
+	protected Node<E> head, tail;
+	protected int size = 0; // Number of elements in the list
+
+	/** Create an empty list */
+	public MyLinkedList() {
+	}
+
+	/** Create a list from an array of objects */
+	public MyLinkedList(E[] objects) {
+		// Left as an exercise
+	}
+
+	/** Return the head element in the list */
+	public E getFirst() {
+		// Left as an exercise
+		final MyLinkedList.Node<E> f = head;
+		if (f == null)
+			throw new NoSuchElementException();
+		return f.element;
+
+	}
+
+	/** Return the last element in the list */
+	public E getLast() {
+		// Left as an exercise
+		final MyLinkedList.Node<E> l = tail;
+		if (l == null)
+			throw new NoSuchElementException();
+		return l.element;
+
+	}
+
+	/** Add an element to the beginning of the list */
+	public void addFirst(E e) {
+		// Left as an exercise
+		final MyLinkedList.Node<E> f = head;
+		final MyLinkedList.Node<E> newNode = new MyLinkedList.Node<>(e);
+		head = newNode;
+		if (f == null)
+			tail = newNode;
+//		else
+//			f.prev = newNode;
+		size++;
+	}
+
+	/** Add an element to the end of the list */
+	public void addLast(E e) {
+		// Left as an exercise
+		final MyLinkedList.Node<E> l = tail;
+		final MyLinkedList.Node<E> newNode = new MyLinkedList.Node<>(e);
+		tail = newNode;
+		if (l == null)
+			head = newNode;
+		else
+			l.next = newNode;
+		size++;
+	}
+
+	@Override /** Add a new element at the specified index 
+	 * in this list. The index of the head element is 0 */
+	public void add(int index, E e) {
+		// Left as an exercise
+	}
+
+	/** Remove the head node and
+	 *  return the object that is contained in the removed node. */
+	public E removeFirst() {
+		// Left as an exercise
+	}
+
+	/** Remove the last node and
+	 * return the object that is contained in the removed node. */
+	public E removeLast() {
+		// Left as an exercise
+	}
+
+	@Override /** Remove the element at the specified position in this 
+	 *  list. Return the element that was removed from the list. */
+	public E remove(int index) {   
+		// Left as an exercise
+	}
+
+	@Override /** Override toString() to return elements in the list */
+	public String toString() {
+		StringBuilder result = new StringBuilder("[");
+
+		Node<E> current = head;
+		for (int i = 0; i < size; i++) {
+			result.append(current.element);
+			current = current.next;
+			if (current != null) {
+				result.append(", "); // Separate two elements with a comma
+			}
+			else {
+				result.append("]"); // Insert the closing ] in the string
+			}
+		}
+
+		return result.toString();
+	}
+
+	@Override /** Clear the list */
+	public void clear() {
+		// Left as an exercise
+	}
+
+	@Override /** Return true if this list contains the element e */
+	public boolean contains(Object e) {
+		// Left as an exercise 
+
+	}
+
+	@Override /** Return the element at the specified index */
+	public E get(int index) {
+		// Left as an exercise 
+
+	}
+
+	@Override /** Return the index of the first matching element in 
+	 *  this list. Return -1 if no match. */
+	public int indexOf(Object e) {
+		// Left as an exercise
+
+	}
+
+	@Override /** Return the index of the last matching element in 
+	 *  this list. Return -1 if no match. */
+	public int lastIndexOf(E e) {
+		// Left as an exercise
+
+	}
+
+	@Override /** Replace the element at the specified position 
+	 *  in this list with the specified element. */
+	public E set(int index, E e) {
+		// Left as an exercise
+
+	}
+
+	@Override /** Override iterator() defined in Iterable */
+	public java.util.Iterator<E> iterator() {
+		return new LinkedListIterator();
+	}
+
+	private class LinkedListIterator 
+	implements java.util.Iterator<E> {
+		private Node<E> current = head; // Current node 
+		private int index=-1; // initial index before head
+
+		@Override
+		public boolean hasNext() {
+			return (current != null);
+		}
+
+		@Override
+		public E next() {
+			E e = current.element;
+			index++;	
+			current = current.next;
+			return e;
+		}
+
+		@Override
+		// remove the last element returned by the iterator
+		public void remove() {
+			MyLinkedList.this.remove(index);	
+		}
+	}
+
+	protected static class Node<E> {
+		// Left as an exercise
+		E element;
+		Node<E> next;
+
+		public Node(E o){
+			this.element = o;
+		}
+	}
+
+	@Override /** Return the number of elements in this list */
+	public int size() {
+		// Left as an exercise
+	}
+}
---
+++ src/LAB/MyQueue.java
@@ -0,0 +1,28 @@
+package LAB;
+
+public class MyQueue<E> {
+  private MyLinkedList<E> list 
+    = new MyLinkedList<E>();
+
+  public void enqueue(E e) {
+    // Left as an exercise
+  }
+
+  public E dequeue() {
+    // Left as an exercise
+  }
+
+  public int getSize() {
+    // Left as an exercise
+  }
+
+  public MyLinkedList<E> getList()
+  {
+	// Left as an exercise
+  }
+  
+  @Override
+  public String toString() {
+    return "Queue: " + list.toString();
+  }
+}
